510e744c45f4d92fa4469a1e05fd99fc63c088f7
Iogsotot <ajustusa@gmail.com>
2020-10-10 21:33:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if ((number = number.toString()).length > 9) return 'overflow';
+    n = ('000' + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+    if (!n) return;
+    let str = '';
+    str = (n[1] == 0 && n[2] == 0) ? 'zero' : '';
+    str += (n[1] != 0 && n[2] != 0) ? a[n[1][0]] + ' ' + 'hundred ' : '';
+    str += (n[1] != 0 && n[2] == 0) ? a[n[1][0]] + ' ' + 'hundred' : '';
+    str += (n[2] != 0 && n[2][1] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) : '';
+    str += (n[2] != 0 && n[2][1] == 0) ? (a[Number(n[2])] || b[n[2][0]] + a[n[2][1]]) : '';
+    return str;
+
 }
